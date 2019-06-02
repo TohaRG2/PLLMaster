e5dc2903c05f3a582473acbdcbe42ef1b88ecc56
biomorf <biomorf.2017@gmail.com>
2019-06-02 15:44:35 +0700

    Try fix fail on JellyBean 4.2.2

--- app/src/main/java/com/biomorf/pllmaster/AboutAppActivity.kt
+++ app/src/main/java/com/biomorf/pllmaster/AboutAppActivity.kt
@@ -3,10 +3,13 @@ package com.biomorf.pllmaster
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class AboutAppActivity : AppCompatActivity() {
+
+class AboutAppActivity : MyDefaultActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about_app)
+
+
     }
 }
